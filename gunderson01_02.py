f4d06e65abd40afb46367c3e4de9718b748e34e2
Evan <evangabe2002@gmail.com>
2022-09-02 14:05:47 -0500

    Assignment 1

---
+++ gunderson01_02.py
@@ -0,0 +1,6 @@
+# Combine morphemes of set words
+
+print("run" + "s")
+print("annoy" + "ing")
+print("un" + "believ" + "able")
+print("stupid" + "ity")
---
+++ gunderson01_1.py
@@ -0,0 +1,5 @@
+2 + 3
+print("Where is the answer?")
+print('Where is the answer?')
+
+# The answer will not be printed as python has not been told to print the answer
