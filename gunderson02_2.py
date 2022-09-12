43d6882cf245a5e95eb6448ec0b78d22bd718845
Evan <evangabe2002@gmail.com>
2022-09-12 14:28:38 -0500

    expand question 2 and create question 4

--- gunderson02_2.py
+++ gunderson02_2.py
@@ -2,6 +2,21 @@
 1, 178, 19, 87]. Submit your script. '''
 
 # To find the second largest number the list should be sorted
+from random import randint
+
+
 my_list = [2, 37, 10, 66, 23, 1, 178, 19, 87] #initializes and assigns list
 my_list.sort() #sorts list smallest to largest
-print(my_list[len(my_list)-2]) #prints the second to last element in the list
+print(my_list)
+print(my_list[-2]) #prints the second to last element in the list
+
+#fun randint version
+flag = 0
+rand_list = []
+while flag !=8:
+    rand_list.append(randint(0,1000))
+    flag += 1
+
+rand_list.sort()
+print(rand_list)
+print(rand_list[-2])
