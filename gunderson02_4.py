9ef1ec611ad308261baaeabd1a1e75fb25f86d68
Evan <evangabe2002@gmail.com>
2022-09-14 14:40:15 -0500

    In class quiz

---
+++ gunderson02_4.py
@@ -0,0 +1,11 @@
+'''Semitic morphology involves intercalating vowels and consonants to express morphological 
+categories. '''
+
+names = ["joe","john","jake","jesus"]
+names2 = names[:]
+names2[1] = "mike"
+names3 = names + names2
+names3.sort()
+
+
+print(names,names2,names3)
--- ling.py
+++ ling.py
@@ -1,11 +1,8 @@
-import tkinter
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+#initialize and assign lists
+list_a = [1,2,3,4,5,6]
+list_b = [3,4,5,6,7,8]
+
+result = set(list_a) & set(list_b) #find the common elements in the two lists
+'''Optional solution in the comment below'''
+# result = set(list_a) -(set(list_a) - set(list_b)) #find the common elements in the two lists below
+print(list(result))
