3d324e01d639bf40eea7c1c648afe0218e8ecd36
Evan <evangabe2002@gmail.com>
2022-09-26 14:12:41 -0500

    a vowel count script w/ input from terminal

--- ling.py
+++ ling.py
@@ -1,11 +1,11 @@
-nouns = 'bla dor sna'.split()
-verbs = 'ha mog ge di'.split()
-ivs = 'ha ge'.split()
+import sys
 
-for s in nouns:
-    for v in nouns:
-        if v in ivs:
-            print(s,v)
-        else:
-            for o in nouns:
-                print(s,v,o)
+vowels = 'aeiou'
+word = sys.argv[1]
+counter = 0
+vowelcount = 0
+while counter < len(word):
+    if word[counter] in vowels:
+        vowelcount += 1
+    counter += 1
+print('There are', vowelcount, 'vowels in this word')
