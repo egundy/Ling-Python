f00d3f612599bf0d6079204988c3bf96449d82f2
Evan <evangabe2002@gmail.com>
2022-10-19 13:26:34 -0500

    change to a function to fulfill

--- gundersonExam1_2.py
+++ gundersonExam1_2.py
@@ -5,11 +5,10 @@ must remove include commas, periods, question marks, hyphens, apostrophes, excla
 colons, and semicolons. Do not remove punctuation marks that appear in the middle of a word, such as 
 the apostrophes used to form a contraction. For example, if your function is provided with the string 
 “Examples of contractions include: don't, isn't, and wouldn't.” then your 
-function should return the list [“Examples', “of”, “contractions”, “include”, 
+function should return the list [“Examples", “of”, “contractions”, “include”, 
 “don't”, “isn't”, “and”, “wouldn't”]. '''
 import re
-
-user_input = input("Enter a string that you want punctuation removed from:\n")
-clean_input = re.sub(r'[^\w\d\s\']+', ' ', user_input)
-
-print("Your cleaned list is:", clean_input.split())
+def clean_text(text):
+    clean_text = re.sub(r'[^\w\d\s\']+', ' ', text)
+    print("Your cleaned list is:", clean_text.split())
+    return clean_text
